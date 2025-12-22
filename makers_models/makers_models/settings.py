f3627b082f9c22e6d92eca15207421e66da08502
Explosion247 <Dave.staward@gmail.com>
2025-12-22 14:18:30 +0000

    added secret key and csrf_trusted_origins

--- makers_models/makers_models/settings.py
+++ makers_models/makers_models/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&xqxo07#znke_(7at6wwfvcn9_vc^rmkybeh)dks$2*@j$nn='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,6 +94,11 @@ DATABASES = {
     'default':
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*makers-models-d9f3c3627d1b.herokuapp.com",
+    "https://*127.0.0.1:8000/",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

4ec31aec3b63cc61ad276732ee8bde1ffb118d7f
Explosion247 <Dave.staward@gmail.com>
2025-12-26 22:50:37 +0000

    - fixed the urls so theyare in the correct order - fixed the like toggle button to allow people to keep track of builds - added a link to the account page

--- makers_models/README.md
+++ makers_models/README.md
@@ -19,3 +19,7 @@ fix - to fix this i separated the inputs into two separate div's and then in the
 error - when loading the reviews, if you had uploaded an image it would not display the image but would show the url
 
 temporary fix - remove the option to upload an image and remove the displaying of the image.
+
+error - when trying to load the accounts page it would come up with 'Page not found'
+
+fix - The urls were in the wrong order so it was matching another page before finding the correct one
--- makers_models/lego_build/urls.py
+++ makers_models/lego_build/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.BuildList.as_view(), name='home'),
+    path('account/', views.liked_builds, name='account'),
     path('<slug:slug>/', views.build_details, name='build_details'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.review_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.delete_review, name='comment_delete'),
-    path('like/<int:build_id>/', views.toggle_like, name='toggle_like'),
+    path('like/<slug:slug>/', views.toggle_like, name='toggle_like'),
+    
 ]

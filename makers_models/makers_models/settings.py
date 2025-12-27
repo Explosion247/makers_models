a805c2ae9b1087eba7fa4a02e64928b18a22fcb4
Explosion247 <Dave.staward@gmail.com>
2025-12-27 10:17:43 +0000

    changed debug to false

--- makers_models/makers_models/settings.py
+++ makers_models/makers_models/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'makers-models-e5ef4f65df4a.herokuapp.com',

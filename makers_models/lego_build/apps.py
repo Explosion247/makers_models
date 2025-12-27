345f9c2da84f4bbf6acde633b2581cc343b6c5ce
Explosion247 <Dave.staward@gmail.com>
2025-12-27 10:37:23 +0000

    reorganised file structure to bring everything into the correct directory

--- makers_models/lego_build/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LegoBuildConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lego_build'
--- makers_models/lego_build/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ReviewModel
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = ReviewModel
-        fields = ('body', 'rating')

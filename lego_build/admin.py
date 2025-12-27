51f30bde8a3fdbcebd70c763c7f0566234774ed8
Explosion247 <Dave.staward@gmail.com>
2025-12-27 10:38:32 +0000

    reorganise file directory

---
+++ lego_build/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Build, ReviewModel
+
+# Register your models here.
+
+@admin.register(Build)
+class BuildAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+admin.site.register(ReviewModel)
